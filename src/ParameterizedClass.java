64de69f3c08c1b8bebb151f2762f2ca62b0650b2
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-11-06 12:33:05 +0200

    Create ParameterizedClass.java

---
+++ src/ParameterizedClass.java
@@ -0,0 +1,25 @@
+public class ParameterizedClass {
+    public static void main(String[] args) {
+        Info <String> info1 = new Info <> ("salut");
+        System.out.println(info1);
+        String s = info1.getValue();
+
+        Info <Integer> info2 = new Info <> (18);
+        System.out.println(info2);
+        Integer s1 = info2.getValue();
+
+    }
+}
+class Info<T>{
+    private T value;
+    public Info (T value){
+        this.value = value;
+    }
+    public String toString(){
+        return "{[" + value + "]}";
+    }
+    public T getValue(){
+        return value;
+    }
+}
+
