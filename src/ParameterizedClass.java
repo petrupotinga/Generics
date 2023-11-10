e18f31fff9050d1d81d387e561a8524562fcf722
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-11-10 16:32:24 +0200

    Update ParameterizedClass.java

--- src/ParameterizedClass.java
+++ src/ParameterizedClass.java
@@ -1,25 +1,47 @@
 public class ParameterizedClass {
     public static void main(String[] args) {
-        Info <String> info1 = new Info <> ("salut");
-        System.out.println(info1);
-        String s = info1.getValue();
-
-        Info <Integer> info2 = new Info <> (18);
-        System.out.println(info2);
-        Integer s1 = info2.getValue();
+//        Info <String> info1 = new Info <> ("salut");
+//        System.out.println(info1);
+//        String s = info1.getValue();
+//
+//        Info <Integer> info2 = new Info <> (18);
+//        System.out.println(info2);
+//        Integer s1 = info2.getValue();
 
     }
+//    public void abc(Info<String> info){
+//        String s = info.getValue();
+//    }
+//    public void abc(Info<Integer> info){
+//        Integer i = info.getValue();
+//    }
+//    abc(Info info)
 }
-class Info<T>{
+
+class Info<T> {
     private T value;
-    public Info (T value){
+
+    public Info(T value) {
         this.value = value;
     }
-    public String toString(){
+
+    public String toString() {
         return "{[" + value + "]}";
     }
-    public T getValue(){
+
+    public T getValue() {
         return value;
     }
 }
 
+class Parent {
+    public void abc(Info<String> info) {
+        String s = info.getValue();
+    }
+}
+
+//class Child extends Parent{
+//    public void abc(Info<Integer> info) {
+//        Integer i = info.getValue();
+//    }
+//}
